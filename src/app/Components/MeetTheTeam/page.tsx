80f1a0a3c16e826d1466a3b70c1f0a1709959744
Mishal Aslam <mishalaslam17@gmail.com>
2024-05-05 18:21:09 +0500

    ...

---
+++ src/app/Components/MeetTheTeam/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GlobalTeam from './GlobbalTeam'
+
+const page = () => {
+  return (
+    <div>
+        <GlobalTeam />
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/Components/Sustainablity/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './Hero'
+import SustainabilityApproach from './SustainabilityApproach'
+import SustainabilityHighlights from './SustainabilityHighlights'
+
+const page = () => {
+  return (
+    <div>
+        <Hero />
+        <SustainabilityApproach />
+        <SustainabilityHighlights />
+    </div>
+  )
+}
+
+export default page
