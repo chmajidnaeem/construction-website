cf5c9a56943d48b88496a03ea02d80d27874f46e
Mishal Aslam <mishalaslam17@gmail.com>
2024-05-05 17:44:59 +0500

    ...

--- src/app/Components/WhoWeAre/OurHistory.tsx
+++ src/app/Components/WhoWeAre/OurHistory.tsx
@@ -48,7 +48,7 @@ export default function OurHistory() {
 
   // This list contains all the data for carousels
   // This can be static or loaded from a server
-  const cards = [
+  const History = [
     {
       title: "2003",
       text: "Tétris founded, in France, by Franck Eburderie.",
@@ -72,6 +72,8 @@ export default function OurHistory() {
       <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-4xl dark:text-white pb-16 pl-12">
         Our History
       </h2>
+      <hr className="border-dashed border-white border-1 mb-12 mx-14" />
+
       {/* Left Icon */}
       <IconButton
         aria-label="left-arrow"
@@ -125,17 +127,17 @@ export default function OurHistory() {
         />
 
         <Slider {...settings} ref={(slider) => setSlider(slider)}>
-          {cards.map((card, index) => (
+          {History.map((history, index) => (
             <Box key={index} height={"100%"} pr={20}>
               <Stack spacing={6} direction="column" height={"100%"} p={2}>
                 <Heading
                   fontSize={{ base: "1xl", md: "2xl", lg: "3xl" }}
                   color={"white"}
                 >
-                  {card.title}
+                  {history.title}
                 </Heading>
                 <Text fontSize={{ base: "sm", lg: "md" }} color="white">
-                  {card.text}
+                  {history.text}
                 </Text>
               </Stack>
             </Box>
